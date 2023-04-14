ad1105fca91bf9f2d1958ce2c53473b285427cca
mlpoo <113955016+mlpoo@users.noreply.github.com>
2023-04-14 11:39:47 +0900

    20230414b
    [Stage Scene]
    -아이템 줍기

--- Assets/Scripts/StageScene/CharacterManager.cs
+++ Assets/Scripts/StageScene/CharacterManager.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using CK_Tutorial_GameJam_April.StageScene.Inventory.Item;
 using CK_Tutorial_GameJam_April.StageScene.Inventory.Slots;
 using UnityEngine;
 
 namespace CK_Tutorial_GameJam_April
 {
 	/// <summary>
-	/// Description
+	/// 캐릭터를 관리하는 스크립트
 	/// </summary>
 	public class CharacterManager : MonoBehaviour
 	{
+		[SerializeField]
 		private SlotsManager slotsManager;
+		
+		[SerializeField]
+		private ItemManager itemManager;
 		
 		[SerializeField]
 		private float speed;
@@ -28,11 +33,11 @@ namespace CK_Tutorial_GameJam_April
 
 		private Vector2 movement;
 
+		private ItemCode itemCode;
+
 		private bool isJumpable = true;
 
-		//private bool isTrigger = false;
-		
-		
+		private bool isTrigger = false;
 
 		private void Awake()
 		{
@@ -42,6 +47,7 @@ namespace CK_Tutorial_GameJam_April
 
 		private void Update()
 		{
+			
 			if (Input.GetKey(KeyCode.A) && rd.velocity.x>maxSpeed * (-1))
 			{
 				rd.AddForce(new Vector2(speed * (-1) * Time.deltaTime, 0f));
@@ -59,6 +65,7 @@ namespace CK_Tutorial_GameJam_April
 			if (Input.GetMouseButton(0)) // 2초간 클릭하면 아이템이 먹어지므로 time에 deltatime을 더해서 구함
 			{
 				time += Time.deltaTime;
+				
 			}
 
 			if (Input.GetMouseButtonUp(0)) // 마우스 클릭을 그만하면 time을 0으로 초기화
@@ -66,29 +73,49 @@ namespace CK_Tutorial_GameJam_April
 				time = 0f;
 			}
 
-			//if (time >= 2f && isTrigger) // 만약 isTrigger상태이고, 2초이상 클릭했다면 아이템을 획득
-			//{
-			//	time = 0f;
-			//	slotsManager.SetTabActive(true);
-			//}
+			if (time >= 2f && isTrigger) // 만약 isTrigger상태이고, 2초이상 클릭했다면 아이템을 획득
+			{
+				time = 0f;
+				slotsManager.SetTabActive(true);
+				itemManager.SetCurrentItem(itemCode.itemID);
+				Destroy(itemCode.gameObject);
+			}
 		}
 		
-		//private void OnCollisionEnter2D(Collision2D other)
-		//{
-		//	if (other.gameObject.name == "Main Tilemap")
-		//	{
-		//		isJumpable = true;
-		//		Debug.Log("can jump");
-		//	}
-		//}
-		//
-		//private void OnCollisionExit2D(Collision2D other)
-		//{
-		//	if (other.gameObject.name == "Main Tilemap")
-		//	{
-		//		isJumpable = false;
-		//		Debug.Log("can't jump");
-		//	}	
-		//}
+		private void OnCollisionEnter2D(Collision2D other)
+		{
+			if (other.gameObject.name == "Main Tilemap")
+			{
+				isJumpable = true;
+				Debug.Log("can jump");
+			}
+		}
+		
+		private void OnCollisionExit2D(Collision2D other)
+		{
+			if (other.gameObject.name == "Main Tilemap")
+			{
+				isJumpable = false;
+				Debug.Log("can't jump");
+			}	
+		}
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject.tag == "Item")
+			{
+				isTrigger = true;
+				itemCode = other.GetComponent<ItemCode>();
+			}
+		}
+
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			if (other.gameObject.tag == "Item")
+			{
+				isTrigger = false;
+			}
+		}
+
 	}
 }
---
+++ Assets/Scripts/StageScene/ItemCode.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace CK_Tutorial_GameJam_April
+{
+	/// <summary>
+	/// 아이템 코드를 관리하는 스크립트
+	/// </summary>
+	public class ItemCode : MonoBehaviour
+	{
+		[SerializeField]
+		public int itemID;
+	}
+}
