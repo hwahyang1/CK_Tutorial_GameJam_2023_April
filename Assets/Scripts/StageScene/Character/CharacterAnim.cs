d1a2eabc26b49e3e16d6381505c6c886d5e4e947
mlpoo <113955016+mlpoo@users.noreply.github.com>
2023-04-19 00:00:06 +0900

    20230418c
    [Stage Scene]
    - 캐릭터의 상태에 따른 이미지 교체
    - 애니메이션 추가
    
    issue
    - 애니메니션 작업을 하면서 y값을 건드려 캐릭터가 움직이지 않음

---
+++ Assets/Scripts/StageScene/Character/CharacterAnim.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CK_Tutorial_GameJam_April
+{
+	/// <summary>
+	/// 캐릭터 애니매이션 관리
+	/// </summary>
+	public class CharacterAnim : MonoBehaviour
+	{
+		private SpriteRenderer spriteRenderer;
+
+		[SerializeField]
+		private Sprite[] Walk = new Sprite[3];
+
+		[SerializeField]
+		private Sprite[] Eat = new Sprite[3];
+
+		[SerializeField]
+		private Sprite pickUp;
+
+		[SerializeField]
+		private Sprite suprized;
+
+		private float time = 0f;
+
+		[SerializeField]
+		private bool onWalk = false;
+		[SerializeField]
+		private bool onEat = false;
+		[SerializeField]
+		private bool onPickUp = false;
+		[SerializeField]
+		private bool onSuprized = false;
+
+		private void Start()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		private void Update()
+		{
+			if (onWalk)
+			{
+				time += Time.deltaTime;
+				if (time > 0f && time <= 0.5f)
+				{
+					spriteRenderer.sprite = Walk[0];
+				}
+				else if (time > 0.5f && time <= 1f)
+				{
+					spriteRenderer.sprite = Walk[1];
+				}
+				else if (time > 1f && time <= 1.5f)
+				{
+					spriteRenderer.sprite = Walk[2];
+				}
+				else
+				{
+					time = 0f; // 루프되야하니까
+				}
+			}
+			else if (onEat)
+			{
+				time += Time.deltaTime;
+				if (time > 0f && time <= 0.5f)
+				{
+					spriteRenderer.sprite = Eat[0];
+				}
+				else if (time > 0.5f && time <= 1f)
+				{
+					spriteRenderer.sprite = Eat[1];
+				}
+				else if (time > 1f && time <= 1.5f)
+				{
+					spriteRenderer.sprite = Eat[2];
+				}
+				else
+				{
+					time = 0f; // 루프되야하니까
+					onEat = false;
+				}
+			}
+			else if (onPickUp)
+			{
+				time += Time.deltaTime;
+				spriteRenderer.sprite = pickUp;
+				if (time > 0.5f)
+				{
+					time = 0f; // 루프되야하니까
+					onPickUp = false;
+				}
+			}
+			else if (onSuprized)
+			{
+				time += Time.deltaTime;
+				spriteRenderer.sprite = suprized;
+				if (time > 0.5f)
+				{
+					time = 0f; // 루프되야하니까
+					onSuprized = false;
+				}
+			}
+			else
+			{
+				time = 0f;
+			}
+		}
+	}
+}
