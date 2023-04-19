31f3696a3d409505b82774ae558c778a71679cbd
HwaHyang <me@hwahyang.space>
2023-04-19 20:54:56 +0900

    20230419i
    [공통]
    - 이미지 추가 (작업 전)

---
+++ Assets/Scripts/CreditScene/CreditParams.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace CK_Tutorial_GameJam_April.CreditScene
+{
+	/// <summary>
+	/// Description
+	/// </summary>
+	public class CreditParams : SingleTon<CreditParams>
+	{
+		
+	}
+}
